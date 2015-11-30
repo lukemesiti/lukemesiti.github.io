31e07fbe155bcc7813ed9368083886832a9af67f
lukemesiti <lukemesiti@gmail.com>
2015-11-30 21:14:36 +1100

    create partials to organise scss remove unnecessary files update gulp file to minify css

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,16 @@
 
 var gulp = require('gulp'),
 		sass = require('gulp-ruby-sass'),
+		minifycss = require('gulp-minify-css'),
+		rename = require('gulp-rename'),
 		autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
 	return sass('styles/*.scss', { style: 'expanded' })
 		.pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+		.pipe(gulp.dest('styles/css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
 		.pipe(gulp.dest('styles/css'));
 });
 
